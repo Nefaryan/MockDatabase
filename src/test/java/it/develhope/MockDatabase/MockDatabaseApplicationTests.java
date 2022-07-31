b867082349c54531e78b9d0ac341aa197fc5f935
Giuseppe Roberti <giuseppe.roberti@informationline.it>
2022-07-31 11:20:57 +0200

    Initial commit

---
+++ src/test/java/it/develhope/MockDatabase/MockDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package it.develhope.MockDatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
